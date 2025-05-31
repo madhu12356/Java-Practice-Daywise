5c3cd4c417b3338cd648f6090896b112d5de219c
MADHAVI GUNDUBILLI <madhavi.gundubilli@gmail.com>
2025-05-31 16:08:11 +0530

    Add files via upload

---
+++ Operations/src/com/Arthamatic.java
@@ -0,0 +1,32 @@
+package com;
+
+public class Arthamatic {
+	public static void main(String[] args) {
+		int a =10;
+		int b =20;
+		int c =30;
+		int d =40;
+		//float varible
+		float f = 5.0f;
+		
+		//int variables 
+		//10 + 20 -> 30 Arithmetic operations on two int variable
+		System.out.println(a+"+"+b+"->"+(a+b));
+		System.out.println(a+"-"+b+"->"+(a-b));
+		System.out.println(a+"*"+b+"->"+(a*b));
+		System.out.println(a+"/"+b+"->"+(a/b));
+		System.out.println(a+"%"+b+"->"+(a%b));
+		// float variable  
+		System.out.println(a+"+"+f+"->"+(a+f));
+		System.out.println(a+"-"+f+"->"+(a-f));
+		System.out.println(a+"*"+f+"->"+(a*f));
+		System.out.println(a+"/"+f+"->"+(a/f));
+		System.out.println(a+"%"+f+"->"+(a%f));
+			
+				
+		
+	}
+	
+	}
+
+
